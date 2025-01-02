b20351761253f55097fd7184441b96f5bca99c1b
GregorySaison <gsaison@apizr.fr>
2025-01-02 21:38:54 +0100

    feat: add routing logic with mock pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+// TODO: Add content to the Home page
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur mon portfolio</h1>
+      <p>Explorez mes projets, publications et plus encore.</p>
+      <p>In progress...</p>
+    </div>
+  );
+};
+  
+export default Home;
+  
---
+++ src/routes/routes.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { Suspense } from "react";
+
+const Home = React.lazy(() => import("../pages/Home"));
+
+const AppRoutes = () => {
+  return (
+    <Router>
+        <Suspense fallback={<div>Chargement...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* Add more routes here during development of the project */}
+          </Routes>
+        </Suspense>
+    </Router>
+  );
+};
+
+export default AppRoutes;
