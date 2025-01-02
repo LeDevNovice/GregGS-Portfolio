5ff60b1d00f68f1c1f0124599a3fa980c3ebfae9
GregorySaison <gsaison@apizr.fr>
2025-01-02 21:59:48 +0100

    feat: add title in homepage with styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,9 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div className="homeContainer">
-      <h1 className="homeContainer__title">
-        Greg<span className="homeContainer__title-dot">.</span>GS
+    <div className="home">
+      <h1 className="home__title">
+        Greg<span className="home__title-dot">.</span>GS
       </h1>  
     </div>
   );
