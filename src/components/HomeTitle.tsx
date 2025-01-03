bbdd9ced365a7ebeae69fea2a2fce8992fe3c9c6
GregorySaison <gsaison@apizr.fr>
2025-01-03 21:03:40 +0100

    feat: add intro animation on title

--- src/components/HomeTitle.tsx
+++ src/components/HomeTitle.tsx
@@ -4,14 +4,37 @@ import '../styles/Home.css';
 
 const HomeTitle = () => {
   return (
-    <motion.h1 
-      className="home__title"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 5, delay: 1, ease: "linear" }}
-    >
-      Greg<span className="home__title-dot">.</span>GS
-    </motion.h1> 
+    <div className="home__container">
+      <motion.h1 
+        className="home__title"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 5, delay: 1, ease: "linear" }}
+      >
+        Greg<motion.span 
+          className="home__title-dot"
+          initial={{ scale: 1 }}
+          animate={{
+            scale: [1, 1.5, 1, 1.5, 1, 1, 500], 
+            backgroundColor: ["#792262", "#792262", "#792262", "#792262", "#792262"],
+            transitionEnd: {
+              scale: 500, 
+            },
+          }}
+          transition={{
+            times: [0, 0.1, 0.2, 0.3, 0.4, 0.8, 3], 
+            duration: 3.5, 
+            delay: 6,
+          }}
+          style={{
+            display: 'inline-block',
+            width: '0.2em',
+            height: '0.2em',
+            borderRadius: '50%',
+          }}
+        ></motion.span>GS
+      </motion.h1>
+    </div>
   );
 };
 
