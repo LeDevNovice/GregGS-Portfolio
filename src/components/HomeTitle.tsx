00b479803bee52b6b9740853f44f4b77db981a73
GregorySaison <gsaison@apizr.fr>
2025-01-05 13:22:22 +0100

    feat: add expand dot animation

--- src/components/HomeTitle.tsx
+++ src/components/HomeTitle.tsx
@@ -1,16 +1,35 @@
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 
 import '../styles/Home.css';
 
 const HomeTitle = () => {
+  const [startDotAnimation, setStartDotAnimation] = useState(false);
+
   return (
     <motion.h1 
       className="home__title"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 5, delay: 1, ease: "linear" }}
+      onAnimationComplete={() => {
+        setStartDotAnimation(true);
+      }}
     >
-      Greg<span className="home__title-dot">.</span>GS
+      Greg<motion.span 
+        className="home__title-dot"
+        initial={{ scale: 1 }}
+        animate={
+          startDotAnimation
+            ? { scale: 300 }
+            : {}
+        }
+        transition={{ 
+          duration: 3, 
+          ease: 'easeIn' 
+        }}
+      >
+      </motion.span>GS
     </motion.h1> 
   );
 };
