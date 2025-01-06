6fd1a9f6fdacf3ea30ec4834bc046edee61f67b8
GregorySaison <gsaison@apizr.fr>
2025-01-06 20:35:18 +0100

    feat: implement footer

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,9 +1,10 @@
+import Footer from "./Footer";
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the home page!</p>
-    </div>
+    <>
+      <Footer />
+    </>
   );
 }
 
