0a46fea4833a30031f53dd1db2d7a7a625c6a2b6
GregorySaison <gsaison@apizr.fr>
2025-01-06 21:38:15 +0100

    feat: add title section on the homepage

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,14 +1,16 @@
 import HomePageBackground from "./HomePageBackground";
 import HomePageFooter from "./HomePageFooter";
 import HomePageSocials from "./HomePageSocials";
+import HomePageTitle from "./HomePageTitle";
 
 const HomePage = () => {
   return (
-    <>
+    <div className="homepage">
+      <HomePageTitle />
       <HomePageBackground />
       <HomePageSocials />
       <HomePageFooter />
-    </>
+    </div>
   );
 }
 
