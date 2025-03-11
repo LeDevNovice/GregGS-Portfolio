46375048443a93d5956a771ed10411ddb11446d7
GregorySaison <gsaison@apizr.fr>
2025-03-11 16:30:06 +0100

    fix: handle perfect stop at the end of the animation

--- src/components/HomePage/HomePageBackground.tsx
+++ src/components/HomePage/HomePageBackground.tsx
@@ -12,8 +12,8 @@ function HomePageBackground({ animateExit }: Props) {
     <motion.img
       src={HomePageBackgroundImage}
       className='homepage__background'
-      initial={{ x: '0%', opacity: 1 }}
-      animate={animateExit ? { x: '-50%', opacity: 0 } : { x: '0%', opacity: 1 }}
+      initial={{ x: '0vw', opacity: 1 }}
+      animate={animateExit ? { x: '-25vw', opacity: 0 } : { x: '0vw', opacity: 1 }}
       transition={{ duration: 3, ease: [0.11, 0, 0.5, 0] }}
     />
   );
